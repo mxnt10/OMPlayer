f1027bd31a0826b768a0eaaf1bd7ea0b3af1d161
mauricio <m10ferrari1200@gmail.com>
2022-04-24 11:34:06 -0300

    Update version 1.1 - 2022/04/24

--- src/Player.h
+++ src/Player.h
@@ -44,87 +44,62 @@ class VideoPlayer : public QWidget {
 Q_OBJECT
 public:
     explicit VideoPlayer(QWidget *parent = nullptr);
-
     ~VideoPlayer() override;
 
 public Q_SLOTS:
-
-    void openMedia(const QStringList &parms = QStringList());
-
     static void blockScreenSaver();
-
+    void openMedia(const QStringList &parms = QStringList());
     void setSelect(int item);
-
     void firstPlay(const QString &name);
-
     void play(const QString &name);
-
     void Next();
-
     void Previous();
-
-    void seekBySlider(int value);
-
+    void setReplay();
+    void setShuffle();
     void seekBySlider();
-
+    void seekBySlider(int value);
     void ShowContextMenu(const QPoint &pos);
 
 private Q_SLOTS:
-
     void playPause();
-
     void setStop();
-
+    void nextRand();
     void onPaused(bool p);
-
     void onStart();
-
     void onStop();
-
     void setHide();
-
     void hideTrue();
-
     void hideFalse();
-
     void setAbout();
-
     void changeFullScreen();
-
     void enterFullScreen();
-
     void leaveFullScreen();
-
     void updateSlider(qint64 value);
-
     void updateSliderUnit();
 
 protected:
     bool event(QEvent *event) override;
-
     void mouseReleaseEvent(QMouseEvent *event) override;
-
     void mouseDoubleClickEvent(QMouseEvent *event) override;
-
     void enterEvent(QEvent *event) override;
-
     void leaveEvent(QEvent *event) override;
-
     void closeEvent(QCloseEvent *event) override;
 
 private:
     AVPlayer *mediaPlayer;
     Button *playBtn, *stopBtn, *nextBtn, *previousBtn;
+    Button *replayBtn, *shuffleBtn;
     MediaInfo MI;
     PlayList *playlist;
     QLabel *logo;
+    QStringList listnum;
     QTimer *noscreensaver;
     QWidget *about, *wctl;
     Slider *slider;
     VideoOutput *video;
     int mUnit;
     int previousitem, actualitem, nextitem;
-    bool restart;
+    bool restart, randplay;
     bool contextmenu, enterpos, maximize, moving, playing, setinfo;
     const char *theme;
 };

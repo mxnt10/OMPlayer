94677302f70f7967511d7a9f5ccef3f207f0915c
mauricio <m10ferrari1200@gmail.com>
2022-04-24 17:36:30 -0300

    Update version 1.1 - 2022/04/24

--- src/ListView.cpp
+++ src/ListView.cpp
@@ -1,6 +1,10 @@
 #include <QDebug>
+#include <QGraphicsOpacityEffect>
+
+#include "Defines.h"
 #include "ListView.h"
 #include "PlayListDelegate.h"
+#include "Utils.h"
 
 
 /**********************************************************************************************************************/
@@ -11,7 +15,13 @@ ListView::ListView() {
     setItemDelegate(new PlayListDelegate(this));
     setSelectionMode(QAbstractItemView::ExtendedSelection); /** Uso com CTRL/SHIF */
     setEditTriggers(QAbstractItemView::NoEditTriggers);
+    setStyleSheet(Utils::setStyle("playlist"));
     setFixedWidth(280);
+
+    /** Efeito semitransparente para o fundo da playlist */
+    auto *effect = new QGraphicsOpacityEffect();
+    effect->setOpacity(OPACY);
+    setGraphicsEffect(effect);
 }
 
 

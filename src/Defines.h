7bad85acaeb46c0591a94dc8f5c0e0583c381366
mauricio <m10ferrari1200@gmail.com>
2022-05-03 21:54:30 -0300

    Upload version 1.3 - 2022/05/02

--- src/Defines.h
+++ src/Defines.h
@@ -6,6 +6,29 @@
 #define DEBUG false
 
 
+/** Definição de cores para o Debug em RGB: \x1b[38;2;000;000;000m */
+#define GRE "\x1b[38;2;000;255;000m" //Debug
+#define RED "\x1b[38;2;255;050;050m" //Debug
+
+#define CYA "\x1b[38;2;000;255;255m" //Interface
+
+#define DGR "\x1b[38;2;000;150;020m" //Eventos
+#define YEL "\x1b[38;2;255;255;000m" //Eventos
+
+#define BLU "\x1b[38;2;000;120;255m" //Outros
+
+#define VIO "\x1b[38;2;120;070;255m" //Posicionamentos
+#define ORA "\x1b[38;2;255;100;000m" //Ações
+
+#define EST "\x1b[38;2;180;125;000m" //Estilo
+
+#define HID "\x1b[38;2;170;255;000m" //Ocultação
+#define SHW "\x1b[38;2;085;255;050m" //Ocultação
+
+#define ADD "\x1b[38;2;255;100;100m" //Adicionar
+#define UPD "\x1b[38;2;100;100;255m" //Atualizar
+
+
 /** Defines para o sobre */
 #define PRG_NAME "Open Multimedia Player"
 #define PRG_DESC "OMPlayer - Open Multimedia Player"

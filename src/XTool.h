8e1b8352a6c459f9fab11e85dc8cfd0b1cb852ea
mauricio <m10ferrari1200@gmail.com>
2022-06-16 21:51:25 -0300

    Update version 1.4.1 - 2022/06/16

--- src/XTool.h
+++ src/XTool.h
@@ -1,12 +1,24 @@
-#ifndef OMPLAYER_XTOOL_H
-#define OMPLAYER_XTOOL_H
+#include <unistd.h>
+#include <X11/Xlib.h>
+#include <X11/extensions/XTest.h>
 
-class XTool {
-public:
-    static void eventMouse(int x, int y);
 
-private:
-    static void mouseClick();
-};
+/**********************************************************************************************************************/
 
-#endif //OMPLAYER_XTOOL_H
+
+/** Função que vai efetuar o clique */
+void mouseClick() {
+    Display *display = XOpenDisplay(nullptr);
+
+    /** Pressionando o mouse */
+    XTestFakeButtonEvent(display, 1, true, 0);
+    XFlush(display);
+
+    usleep(10000);
+    
+    /** Soltando o botão do mouse */
+    XTestFakeButtonEvent(display, 1, false, 0);
+    XFlush(display);
+
+    XCloseDisplay(display);
+}

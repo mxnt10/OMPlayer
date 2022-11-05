008a42240e82a17c08b2a4d84c3314d045069e06
mauricio <m10ferrari1200@gmail.com>
2022-11-05 19:51:33 -0300

    Update version 1.6 - 2022/11/05

--- src/MessageReceiver.cpp
+++ src/MessageReceiver.cpp
@@ -12,5 +12,5 @@ MessageReceiver::MessageReceiver(QObject *parent) : QObject(parent) {}
 /** Função que recebe os parâmetros da instâncias secundárias */
 void MessageReceiver::receivedMessage(int id, const QByteArray& message) {
     qDebug("%s(%sMessageReceiver%s)%s::%sArgumentos recebidos da instância %i ...\033[m", GRE, RED, GRE, RED, BLU, id);
-    emit parms(QString::fromUtf8(message).split('\n'));
+    Q_EMIT parms(QString::fromUtf8(message).split('\n'));
 }

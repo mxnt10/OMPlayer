11f1f9c4a8e217496c2cf0e6d5ace14cdf3c189e
mauricio <m10ferrari1200@gmail.com>
2022-11-12 18:07:33 -0300

    Update version 2.0 - 2022/11/10

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ cd ..
 install -Dm 0755 bin/OMPlayer "$install_root"/usr/bin
 install -Dm 0644 appdata/OMPlayer.png "$install_root"/usr/share/pixmaps
 install -Dm 0644 appdata/logo.png "$install_root"/usr/share/OMPlayer/logo
+install -Dm 0644 appdata/notify.png "$install_root"/usr/share/OMPlayer/logo
 install -Dm 0644 appdata/OMPlayer.desktop "$install_root"/usr/share/applications
 
 cp -a ChangeLog LICENSE README.md "$install_root"/usr/doc/OMPlayer-"$pkgver"

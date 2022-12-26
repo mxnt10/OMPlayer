acc3e4daa6a78938121776f2682438328b5202e0
mauricio <m10ferrari1200@gmail.com>
2022-12-25 22:23:47 -0300

    Upload version 2.0.1 - 2022/12/25

---
+++ src/StatisticsWorker.h
@@ -0,0 +1,38 @@
+#ifndef OMPLAYER_STATISTICSWORKER_H
+#define OMPLAYER_STATISTICSWORKER_H
+
+#include <QObject>
+#include <MediaInfoDLL.h>
+#include <QtAV/Statistics.h>
+
+class StatisticsWorker : public QObject {
+Q_OBJECT
+
+public:
+    explicit StatisticsWorker(QObject *parent = nullptr);
+    void requestWork();
+
+    void setFile(const QString &val, const QtAV::Statistics &s) { file = val; statistics = s; };
+
+Q_SIGNALS:
+    void baseValues(const QStringList &values, const QStringList &valuesVideo);
+    void workRequested();
+    void finished();
+
+public Q_SLOTS:
+    void doWork();
+
+private:
+    static QString setFormat(const QString &format);
+    static QString convertByte(auto byte);
+    static QString convertBit(auto bit);
+    static QString convertAspectRatio(int x, int y);
+
+    QString file{};
+    QtAV::Statistics statistics{};
+    MediaInfoDLL::MediaInfo MI{};
+
+
+};
+
+#endif //OMPLAYER_STATISTICSWORKER_H
